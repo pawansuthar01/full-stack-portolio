3de0160872e7583d83aba383c62912578da83ec0
pawansuthar01 <paw.kum.2111@gmail.com>
2025-02-26 20:11:51 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,8 @@ import { config } from "dotenv";
 config();
 import cloudinaryPkg from "cloudinary";
 const { v2: cloudinary } = cloudinaryPkg;
-console.log(config());
 import app from "./App.js";
 const PORT = process.env.PORT || 5000;
-console.log(PORT);
 export const Cloudinary = cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
