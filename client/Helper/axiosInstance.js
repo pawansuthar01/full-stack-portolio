bb8a8056ae3d9e0b1269d57f1f6d8b917f8fe612
pawansuthar01 <paw.kum.2111@gmail.com>
2025-03-18 16:50:19 +0530

    new backend url update in client side

--- client/Helper/axiosInstance.js
+++ client/Helper/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASIC_URL = "https://full-stack-portolio-1.onrender.com";
+const BASIC_URL = "https://full-stack-portolio-1-0evy.onrender.com";
 const axiosInstance = axios.create();
 axiosInstance.defaults.baseURL = BASIC_URL;
 axiosInstance.defaults.withCredentials = true;
